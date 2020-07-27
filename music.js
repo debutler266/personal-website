4f76cb3590bb92198799cf1c3be0acbe048d6759
debutler266 <32465329+debutler266@users.noreply.github.com>
2020-07-27 16:53:11 -0400

    added the music player js using aplayer and css styling to the page

--- music.js
+++ music.js
@@ -1,10 +1,59 @@
 const ap = new APlayer({
     container: document.getElementById('aplayer'),
     listFolded: true,
-    audio: [{
+    audio: [
+      {
         name: 'Creative Minds',
         artist: 'Artist Name',
         url: 'audio1.mp3',
         cover: 'ac1.jpg'
-    }]
+    },
+    {
+      name: 'Summer',
+      artist: 'Artist Name',
+      url: 'audio2.mp3',
+      cover: 'cs1.jpg'
+  },
+  {
+      name: 'Happy Rock',
+      artist: 'Artist Name',
+      url: 'audio3.mp3',
+      cover: 'ac1.jpg'
+},
+{
+    name: 'Memories',
+    artist: 'Artist Name',
+    url: 'audio4.mp3',
+    cover: 'cs1.jpg'
+},
+
+{
+  name: 'Going Higher',
+  artist: 'Artist Name',
+  url: 'audio5.mp3',
+  cover: 'ac1.jpg'
+},
+
+{
+  name: 'Acoustic Breeze',
+  artist: 'Artist Name',
+  url: 'audio6.mp3',
+  cover: 'cs1.jpg'
+},
+
+{
+  name: 'Tomorrow',
+  artist: 'Artist Name',
+  url: 'audio7.mp3',
+  cover: 'ac1.jpg'
+},
+
+{
+  name: 'Slow Motion',
+  artist: 'Artist Name',
+  url: 'audio8.mp3',
+  cover: 'cs1.jpg'
+},
+
+  ]
 });
