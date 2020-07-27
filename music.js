59a318d2294becac43df626a3bcd5ed353e1f025
debutler266 <32465329+debutler266@users.noreply.github.com>
2020-07-27 16:07:29 -0400

    working on beginning stages for music page

---
+++ music.js
@@ -0,0 +1,10 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    listFolded: true,
+    audio: [{
+        name: 'Creative Minds',
+        artist: 'Artist Name',
+        url: 'audio1.mp3',
+        cover: 'ac1.jpg'
+    }]
+});
